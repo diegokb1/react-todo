8de38e68ed6ba5b0af2438cbd5371ce9e3dbb002
Diego Klappenbach <dklappenbach@vairix.com>
2020-07-08 11:36:10 -0300

    Add add todo item functionality and test

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -11,4 +11,18 @@ describe('TodoApp', () => {
     expect(TodoApp).toExist();
   });
 
+  describe('handleAddTodo', () => {
+    it('Adds a new todo item', () => {
+      const todoText = 'test text';
+      const todoApp = TestUtils.renderIntoDocument(<TodoApp />);
+
+      todoApp.setState({ todos: [] });
+      expect(todoApp.state.todos.length).toBe(0);
+      todoApp.handleAddTodo(todoText);
+
+      expect(todoApp.state.todos.length).toBe(1);
+      expect(todoApp.state.todos[0].text).toBe(todoText);
+    });
+  });
+
 });

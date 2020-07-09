9fcb0f3a8094f5691e97c9246f57f3bfd53baaf1
Diego Klappenbach <dklappenbach@vairix.com>
2020-07-09 16:12:43 -0300

    Add actions and action tests

---
+++ app/actions/actions.jsx
@@ -0,0 +1,26 @@
+export const setSearchText = searchText => {
+  return {
+    type: 'SET_SEARCH_TEXT',
+    payload: searchText
+  }
+};
+
+export const addTodo = text => {
+  return {
+    type: 'ADD_TODO',
+    payload: text
+  }
+};
+
+export const toggleShowCompleted = () => {
+  return {
+    type: 'TOGGLE_SHOW_COMPLETED'
+  }
+};
+
+export const toggleTodo = todoId => {
+  return {
+    type: 'TOGGLE_TODO',
+    payload: todoId
+  }
+}
---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,51 @@
+const expect = require('expect');
+const actions = require('actions');
+
+describe('Actions', () => {
+  describe('SetSearchText', () => {
+    it('generates search text action', () => {
+      const action = {
+        type: 'SET_SEARCH_TEXT',
+        payload: 'test text'
+      };
+      const res = actions.setSearchText('test text');
+
+      expect(res).toEqual(action);
+    });
+  });
+
+  describe('addTodo', () => {
+    it('generates add todo action', () => {
+      const action = {
+        type: 'ADD_TODO',
+        payload: 'todo text'
+      };
+      const res = actions.addTodo('todo text');
+
+      expect(res).toEqual(action);
+    });
+  });
+
+  describe('toggleShowCompleted', () => {
+    it('generates toggle show completed action', () => {
+      const action = {
+        type: 'TOGGLE_SHOW_COMPLETED',
+      };
+      const res = actions.toggleShowCompleted();
+
+      expect(res).toEqual(action);
+    });
+  });
+
+  describe('toggleTodo', () => {
+    it('generates toggle todo action', () => {
+      const action = {
+        type: 'TOGGLE_TODO',
+        payload: 4
+      };
+      const res = actions.toggleTodo(4);
+
+      expect(res).toEqual(action);
+    });
+  });
+});
